f9da34a8eb69439c68536b913a16aa0c80b51501
Braincrash <braincrash.nxs@gmail.com>
2014-04-27 19:44:07 +0100

    server for openshift

--- server.js
+++ server.js
@@ -36,7 +36,13 @@ app.get('/*', function (req, res) {
 })
 
 var server;
-exports.ipaddress = process.env.OPENSHIFT_NODEJS_IP || 127.0.0.1;
+exports.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+if (typeof exports.ipaddress == "undefined") {
+//  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+//  allows us to run/test the app locally.
+ console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+ self.ipaddress = "127.0.0.1";  
+};
 exports.port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 exports.start = start;
 exports.stop = stop;
