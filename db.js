ecfe8e528b1fba67b68858bba395f6a506e66d6c
Braincrash <braincrash.nxs@gmail.com>
2014-04-27 20:38:32 +0100

    mysql for openshift

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
-var config = require('konphyg')(__dirname + '/config');
+//var config = require('konphyg')(__dirname + '/config');
 
 var mysql = require('mysql');
-var mysqlConf = config('mysql');
+//var mysqlConf = config('mysql');
 
+/*
 var connection = mysql.createConnection({
   host: mysqlConf.host,
   user: mysqlConf.user,
@@ -10,6 +11,15 @@ var connection = mysql.createConnection({
   database: mysqlConf.database,
   port: mysqlConf.port
 });
+*/
+var connection = mysql.createConnection({
+  host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
+  user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
+  pass     : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+  port     : process.env.OPENSHIFT_MYSQL_DB_PORT,
+  database : 'trulyapi'
+ });
+
 
 connection.connect();
 
